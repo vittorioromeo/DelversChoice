71ff7c5a51c2cc488d24002485f942ddb9444ac0
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-09-27 08:47:00 +0200

    Newline

--- DCServer/NetLayer/src/test.cpp
+++ DCServer/NetLayer/src/test.cpp
@@ -1,5 +1,5 @@
 #include "../include/NetLayer/NetLayer.hpp"
-#include "/home/vittorioromeo/OHWorkspace/private/cpp/cppcon2015/definitive_WIP/Other/Other.hpp"
+#include "/home/vittorioromeo/OHWorkspace/cppcon2015/Other/Other.hpp"
 
 // TODO:
 // * decide abstraction layers
