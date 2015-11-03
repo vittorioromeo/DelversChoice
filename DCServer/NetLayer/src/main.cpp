2f7c9f6b1e64b8e9bafcfd4f6396b0a74552a26e
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-11-03 13:39:34 +0100

    pro

--- DCServer/NetLayer/src/main.cpp
+++ DCServer/NetLayer/src/main.cpp
@@ -102,6 +102,12 @@ namespace example
             (                                                      // .
                              (std::vector<std::string>), messages) // .
             );
+
+        NL_DEFINE_PCKT(Notify,                   // .
+            (                                    // .
+                           ((int), channel_id),  // .
+                           ((std::string), msg)) // .
+            );
     }
 }
 
@@ -121,10 +127,20 @@ namespace db_actions
         auto result(db()(
             select(all_of(tbl_user)).from(tbl_user).where(tbl_user.id == id)));
 
-        if(result.empty())
-        {
-            return false;
-        }
+        if(result.empty()) return false;
+
+        f(result.front());
+        return true;
+    }
+
+    template <typename TF>
+    bool user_by_username(const std::string& username, TF&& f)
+    {
+        auto result(db()(select(all_of(tbl_user))
+                             .from(tbl_user)
+                             .where(tbl_user.username == username)));
+
+        if(result.empty()) return false;
 
         f(result.front());
         return true;
@@ -137,10 +153,82 @@ namespace db_actions
             });
     }
 
-    auto registration(const to_s::Registration& x)
+    bool has_user_by_username(const std::string& username)
     {
-        return db()(insert_into(tbl_user).set(tbl_user.username = x.user(),
-            tbl_user.pwdHash = utils::hash_pwd(x.pass())));
+        return user_by_username(username, [](const auto&)
+            {
+            });
+    }
+
+    template <typename TF>
+    bool channel_by_id(int id, TF&& f)
+    {
+        auto result(db()(select(all_of(tbl_channel))
+                             .from(tbl_channel)
+                             .where(tbl_channel.id == id)));
+
+        if(result.empty()) return false;
+
+        f(result.front());
+        return true;
+    }
+
+    bool has_channel_by_id(int id)
+    {
+        return channel_by_id(id, [](const auto&)
+            {
+            });
+    }
+
+    auto create_user(const std::string& user, const std::string& pass)
+    {
+        return db()(insert_into(tbl_user).set(tbl_user.username = user,
+            tbl_user.pwdHash = utils::hash_pwd(pass)));
+    }
+
+    auto create_channel(int user_id, const std::string& name)
+    {
+        return db()(
+            insert_into(tbl_channel)
+                .set(tbl_channel.idUser = user_id, tbl_channel.name = name));
+    }
+
+    auto delete_channel(int id)
+    {
+        assert(has_channel_by_id(id));
+        return db()(remove_from(tbl_channel).where(tbl_channel.id == id));
+    }
+
+    auto add_user_to_channel(int user_id, int channel_id)
+    {
+        assert(has_channel_by_id(user_id));
+        assert(has_user_by_id(channel_id));
+
+        return db()(insert_into(tbl_user_channel)
+                        .set(tbl_user_channel.idUser = user_id,
+                            tbl_user_channel.idChannel = channel_id));
+    }
+
+    auto remove_user_from_channel(int user_id, int channel_id)
+    {
+        assert(has_channel_by_id(user_id));
+        assert(has_user_by_id(channel_id));
+
+        return db()(remove_from(tbl_user_channel)
+                        .where(tbl_user_channel.idUser == user_id &&
+                               tbl_user_channel.idChannel == channel_id));
+    }
+
+    auto create_message(
+        int user_id, int channel_id, const std::string& contents)
+    {
+        assert(has_channel_by_id(user_id));
+        assert(has_user_by_id(channel_id));
+
+        return db()(insert_into(tbl_message)
+                        .set(tbl_message.idUser = user_id,
+                            tbl_message.idChannel = channel_id,
+                            tbl_message.contents = contents));
     }
 }
 
@@ -148,192 +236,91 @@ int main()
 {
     initialize_db_connection();
 
+    // auto x =
+    // db_actions::registration(example::to_s::Registration{nl::init_fields{},
+    // "ciao", "Mondo"});
+    // std::cout << x <<"\n";
+
     return 0;
 }
 
-
 namespace example
 {
-    NL_DEFINE_PCKT(
-        RegistrationRequest, (((std::string), user), ((std::string), pass)));
-
-    NL_DEFINE_PCKT_1(RegistrationResponse, ((bool), valid));
-
-    NL_DEFINE_PCKT(
-        LoginRequest, (((std::string), user), ((std::string), pass)));
-
-    NL_DEFINE_PCKT_1(LoginResponse, ((bool), valid));
-    NL_DEFINE_PCKT_1(Message, ((std::string), msg));
-
     namespace nle = experiment;
 
     using MySettings = nle::Settings<nl::UInt32>;
 
-    constexpr auto my_pckt_binds(nle::pckt_binds<RegistrationRequest,
-        LoginRequest, RegistrationResponse, LoginResponse, Message>());
+    constexpr auto my_pckt_binds(
+        nle::pckt_binds<to_s::Registration, to_s::Login, to_s::CreateChannel,
+            to_s::DeleteChannel, to_s::SendMessage, to_s::GetMessages,
+            to_c::Outcome, to_c::Messages, to_c::Notify>());
 
     constexpr auto my_config(nle::make_config<MySettings>(my_pckt_binds));
 
     using MyConfig = decltype(my_config);
 
     using MyContextHost = nle::ContextHost<MyConfig>;
+}
 
-    static_assert(MyConfig::getPcktBindID<RegistrationRequest>() == 0, "");
-    static_assert(MyConfig::getPcktBindID<LoginRequest>() == 1, "");
-    static_assert(MyConfig::getPcktBindID<RegistrationResponse>() == 2, "");
-    static_assert(MyConfig::getPcktBindID<LoginResponse>() == 3, "");
-
-
-    void startServer()
+namespace example
+{
+    class connection_state
     {
-        struct user_data
+    private:
+        static constexpr int max_life{100};
+        nl::PAddress _addr;
+        int _life{max_life};
+        int _id{0};
+
+    public:
+        connection_state(const nl::PAddress& addr) : _addr(addr) {}
+
+        const auto& addr() { return _addr; }
+        const auto& id() { return _id; }
+
+        auto is_logged_in() { return _id > 0; }
+        auto dead() { return _life <= 0; }
+
+        void decrease_life() { --_life; }
+        void reset_life() { _life = max_life; }
+        void set_logged_in_as(int id)
         {
-            nl::PAddress _id;
-            std::string _user;
-            std::string _pass;
+            assert(!is_logged_in());
+            _id = id;
+        }
+    };
 
-            user_data(const nl::PAddress& id, const std::string& user,
-                const std::string& pass)
-                : _id(id), _user(user), _pass(pass)
-            {
-            }
-        };
+    class server_state
+    {
+    private:
+        std::vector<std::unique_ptr<connection_state>> _connections;
 
-        struct login_data
+    public:
+        server_state() = default;
+
+        connection_state* conn_by_addr(const nl::PAddress& x)
         {
-            nl::PAddress _id;
-            int _life{100};
+            for(auto& c : _connections)
+                if(c->addr() == x) return c.get();
 
-            login_data(const nl::PAddress& id) : _id(id) {}
-        };
+            return nullptr;
+        }
 
-        // int next_id = 0;
-        std::vector<user_data> registered_users;
-        std::vector<login_data> logged_in;
-
-        auto user_exists([&](const auto& xuser)
-            {
-                for(const auto& x : registered_users)
-                    if(x._user == xuser) return true;
-
-                return false;
-            });
-
-        auto get_user_by_username([&](const auto& xuser)
-            {
-                for(const auto& x : registered_users)
-                    if(x._user == xuser) return x;
-
-                SSVU_UNREACHABLE();
-            });
-
-        auto is_logged_in([&](const auto& id)
-            {
-                for(const auto& x : logged_in)
-                    if(x._id == id) return true;
-
-                return false;
-            });
-
-        auto get_logged_user_by_id([&](const auto& id) -> login_data&
-            {
-                for(auto& x : logged_in)
-                    if(x._id == id) return x;
-
-                SSVU_UNREACHABLE();
-            });
-
-        MyContextHost h{27015};
-
-        h.on_d<RegistrationRequest>(
-            [&](const auto& sender, const auto& user, const auto& pass)
-            {
-                ssvu::lo() << "registration request from " << user
-                           << ", pass: " << pass << "\n";
-
-                if(!user_exists(user))
-                {
-                    ssvu::lo() << "Replying with OK\n";
-                    registered_users.emplace_back(sender, user, pass);
-
-                    h.send<RegistrationResponse>(sender, true);
-                }
-                else
-                {
-                    ssvu::lo() << "User already exists, replying with NO\n";
-
-                    h.send<RegistrationResponse>(sender, false);
-                }
-            });
-
-        h.on_d<LoginRequest>(
-            [&](const auto& sender, const auto& user, const auto& pass)
-            {
-                ssvu::lo() << "login request from " << user
-                           << ", pass: " << pass << "\n";
-
-                if(!user_exists(user))
-                {
-                    ssvu::lo() << "No such user.\nReplying with NO\n";
-                    return;
-                }
-
-                const auto& u(get_user_by_username(user));
-
-                if(u._pass == pass)
-                {
-                    if(is_logged_in(sender))
-                    {
-                        ssvu::lo() << "Already logged in.\nReplying with NO\n";
-                        h.send<LoginResponse>(sender, false);
-                    }
-                    else
-                    {
-                        ssvu::lo() << "Replying with OK\n";
-                        logged_in.emplace_back(u._id);
-                        h.send<LoginResponse>(sender, true);
-                    }
-                }
-                else
-                {
-                    ssvu::lo() << "Wrong password.\nReplying with NO\n";
-
-                    h.send<LoginResponse>(sender, false);
-                }
-            });
-
-        h.on_d<Message>([&](const auto& sender, const auto& msg)
-            {
-                if(!is_logged_in(sender))
-                {
-                    ssvu::lo() << "Error: not logged in.\n";
-                    return;
-                }
-
-                get_logged_user_by_id(sender)._life = 100;
-
-                ssvu::lo() << "Message from " << sender << ":\n" << msg
-                           << "\n\n";
-            });
-
-        while(h.busy())
+        connection_state* conn_by_id(int id)
         {
-            // Try to process all packets.
-            while(h.try_dispatch_and_process())
-            {
-                // std::cout << "p...\n";
-            }
+            for(auto& c : _connections)
+                if(c->id() == id) return c.get();
 
-            //   std::cout << "NO MORE P...\n";
+            return nullptr;
+        }
 
-            for(auto& x : logged_in)
-            {
-                --x._life;
-            }
+        void decrease_life()
+        {
+            for(auto& c : _connections) c->decrease_life();
 
-            ssvu::eraseRemoveIf(logged_in, [](const auto& x)
+            ssvu::eraseRemoveIf(_connections, [](const auto& x)
                 {
-                    if(x._life <= 0)
+                    if(x->dead())
                     {
                         ssvu::lo() << "Client #" << x._id << " timed out.\n";
                         return true;
@@ -341,27 +328,45 @@ namespace example
 
                     return false;
                 });
-
-            std::this_thread::sleep_for(100ms);
         }
-    }
+    };
+}
 
-    void startClient()
+namespace example
+{
+    void startServer()
     {
-        MyContextHost h{27016};
+        using namespace to_s;
 
-        nl::PAddress serveraddr(nl::IpAddr::getLocalAddress(), 27015);
+        server_state s;
+        MyContextHost h{27015};
 
-        h.on_d<RegistrationResponse>([&](const auto&, const auto& outcome)
+        h.on_d<Registration>(
+            [&](const auto& sender, const auto& user, const auto& pass)
             {
-                ssvu::lo() << "Received registration response, outcome:\n"
-                           << (outcome ? "VALID" : "INVALID") << "\n";
             });
 
-        h.on_d<LoginResponse>([&](const auto&, const auto& outcome)
+        h.on_d<Login>(
+            [&](const auto& sender, const auto& user, const auto& pass)
             {
-                ssvu::lo() << "Received login response, outcome:\n"
-                           << (outcome ? "VALID" : "INVALID") << "\n";
+            });
+
+        h.on_d<CreateChannel>([&](const auto& sender, const auto& name)
+            {
+            });
+
+        h.on_d<DeleteChannel>([&](const auto& sender, const auto& id)
+            {
+            });
+
+        h.on_d<SendMessage>([&](
+            const auto& sender, const auto& channel_id, const auto& contents)
+            {
+            });
+
+        h.on_d<GetMessages>(
+            [&](const auto& sender, const auto& channel_id, const auto& count)
+            {
             });
 
         while(h.busy())
@@ -369,55 +374,148 @@ namespace example
             // Try to process all packets.
             while(h.try_dispatch_and_process())
             {
-                // std::cout << "p...\n";
+            }
+
+            // Timeout.
+            s.decrease_life();
+
+            std::this_thread::sleep_for(100ms);
+        }
+    }
+
+    enum class cs
+    {
+        unlogged,
+        logged,
+        awaiting_channel_list,
+        selecting_channel,
+        awaiting_login_response,
+        awaiting_create_channel_response,
+        awaiting_registration_response,
+    };
+
+    struct client_state
+    {
+        cs s{cs::unlogged};
+    };
+
+    void startClient()
+    {
+        using namespace to_c;
+
+        MyContextHost h{27016};
+        client_state s;
+
+        nl::PAddress serveraddr(nl::IpAddr::getLocalAddress(), 27015);
+
+        h.on_d<Outcome>([&](const auto&, const auto& valid, const auto& msg)
+            {
+                ssvu::lo() << "Received registration response, outcome:\n"
+                           << (valid ? "VALID" : "INVALID")
+                           << "\nMessage: " << msg << "\n\n";
+            });
+
+        h.on_d<Messages>([&](const auto&, const auto& messages)
+            {
+
+            });
+
+        h.on_d<Notify>([&](const auto&, const auto& channel_id, const auto& msg)
+            {
+
+            });
+
+        while(h.busy())
+        {
+            // Try to process all packets.
+            while(h.try_dispatch_and_process())
+            {
             }
 
             // std::cout << "NO MORE P...\n";
 
-
-
-            ssvu::lo("Choose") << "\n"
-                               << "0. Register\n"
-                               << "1. Login\n"
-                               << "2. Send message\n"
-                               << "_. Exit\n";
-
-            auto choice(getInput<int>("Choice"));
-
-            if(choice == 0 || choice == 1)
+            if(s.s == cs::awaiting_login_response)
             {
-                std::string username, password;
+                ssvu::lo() << "Awaiting login response...\n";
+            }
+            else if(s.s == cs::awaiting_create_channel_response)
+            {
+                ssvu::lo() << "Awaiting create channel response...\n";
+            }
+            else if(s.s == cs::awaiting_registration_response)
+            {
+                ssvu::lo() << "Awaiting registration response...\n";
+            }
+            else if(s.s == cs::unlogged)
+            {
+                ssvu::lo("Choose") << "\n"
+                                   << "0. Register\n"
+                                   << "1. Login\n"
+                                   << "_. Exit\n";
 
-                ssvu::lo() << "Insert username:\n";
-                std::cin >> username;
+                auto choice(getInput<int>("Choice"));
 
-                ssvu::lo() << "Insert password:\n";
-                std::cin >> password;
+                if(choice == 0 || choice == 1)
+                {
+                    std::string username, password;
+
+                    ssvu::lo() << "Insert username:\n";
+                    std::cin >> username;
+
+                    ssvu::lo() << "Insert password:\n";
+                    std::cin >> password;
+
+                    if(choice == 0)
+                    {
+                        h.make_and_send<to_s::Registration>(
+                            serveraddr, username, password);
+
+                        s.s = cs::awaiting_registration_response;
+                    }
+                    else if(choice == 1)
+                    {
+                        h.make_and_send<to_s::Login>(
+                            serveraddr, username, password);
+
+                        s.s = cs::awaiting_login_response;
+                    }
+                }
+                else
+                {
+                    h.stop();
+                    break;
+                }
+            }
+            else if(s.s == cs::logged)
+            {
+                ssvu::lo("Choose") << "\n"
+                                   << "0. Create channel\n"
+                                   << "1. Subscribe to channel\n"
+                                   << "2. Send broadcast\n"
+                                   << "_. Logout\n";
+
+                auto choice(getInput<int>("Choice"));
 
                 if(choice == 0)
                 {
-                    h.make_and_send<RegistrationRequest>(
-                        serveraddr, username, password);
+                    auto name = getInputLine("Channel name:");
+                    h.make_and_send<to_s::CreateChannel>(serveraddr, name);
+                    s.s = cs::awaiting_create_channel_response;
                 }
                 else if(choice == 1)
                 {
-                    h.make_and_send<LoginRequest>(
-                        serveraddr, username, password);
+                    h.make_and_send<to_s::ChannelList>(serveraddr);
+                    s.s = cs::awaiting_channel_list;
+                }
+                else if(choice == 2)
+                {
+                }
+                else
+                {
+                    h.make_and_send<to_s::Logout>(serveraddr);
                 }
             }
-            else if(choice == 2)
-            {
-                std::string m;
-                ssvu::lo() << "Insert message:\n";
-                std::cin >> m;
 
-                h.make_and_send<Message>(serveraddr, m);
-            }
-            else
-            {
-                h.stop();
-                break;
-            }
 
             std::this_thread::sleep_for(100ms);
         }
